7731cd6f72c96407d7ac245174f139b4f27a0fec
Emma Danica <ms.bee.indeed@gmail.com>
2020-09-10 05:44:34 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,5 @@
 def greeting(name)
   puts "Hi! I'm HAL, what's your name?"
-  puts "Hello, #{name}. It's nice to meet you."
+  name= gets.strip
+    puts "Hello, #{name}. It's nice to meet you."
 end
